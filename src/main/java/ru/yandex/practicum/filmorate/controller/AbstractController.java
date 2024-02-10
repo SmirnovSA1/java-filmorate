7de1b575c5b041e61b873e5a3260f577e4446c65
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-02-11 01:36:35 +0300

    refactor: удалить контстантное поле BIRTHDATE_FILM.

--- src/main/java/ru/yandex/practicum/filmorate/controller/AbstractController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/AbstractController.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.util.HashMap;
 
 public abstract class AbstractController<T> {
-    protected final LocalDate BIRTHDATE_FILM = LocalDate.of(1895, 12, 28);
     protected HashMap<Integer, Film> films = new HashMap<>();
     protected HashMap<Integer, User> users = new HashMap<>();
 }
