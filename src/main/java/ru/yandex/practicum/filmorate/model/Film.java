48db26549368c1007ae5b6862058afc1f24ad9c0
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-02-11 01:08:08 +0300

    feat: создать класс и добавить поля id, name, description, releaseDate, duration.
    feat: добавить конструкторы, геттеры и сеттеры с помощью анотаций Lombok
    
    feat: добавить анотации полям для валидации spring-validation.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,31 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Film {
+    private Integer id;
+    @NotBlank
+    private String name;
+    @Size(min = 1, max = 200)
+    private String description;
+    @NotNull
+    private LocalDate releaseDate;
+    @Min(1)
+    private long duration;
+    private static int count = 0;
+
+    public void generateId() {
+        this.id = ++count;
+    }
 }
