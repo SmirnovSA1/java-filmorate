3b5be814944c54d7dcde3497da71a291634a8df5
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-02-24 00:54:23 +0300

    feat: добавить поле likes для хранения лайков пользователей (идентификаторы пользователей) в разрезе экземпляра объекта. feat: добавить статичный метод resetCount для сброса счетчика идентификаторов, чтобы unit-тесты проходили без ошибок.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -23,9 +25,13 @@ public class Film {
     private LocalDate releaseDate;
     @Min(1)
     private long duration;
+    private final Set<Integer> likes = new HashSet<>();
     private static int count = 0;
 
     public void generateId() {
         this.id = ++count;
+    }
+    public static void resetCount() {
+        count = 0;
     }
 }
