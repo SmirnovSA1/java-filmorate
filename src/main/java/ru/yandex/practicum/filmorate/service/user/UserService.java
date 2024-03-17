12ca5144f93700fddfc981d0cd096850d13949f5
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-03-17 04:04:11 +0300

    refactor: спринт 11 (ТЗ проекта) - рефакторинг (улучшить работу блока сервиса)

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -17,105 +17,43 @@ public class UserService {
     private final UserStorage userStorage;
 
     public List<User> getUsers() {
-        log.info("Получение списка пользователей");
         return userStorage.getUsers();
     }
 
     public User getUserById(Integer userId) {
-        log.info("Получение пользователя по id: {}", userId);
         return userStorage.getUserById(userId);
     }
 
     public User createUser(User user) {
-        log.info("Создание пользователя: {}", user);
         return userStorage.createUser(user);
     }
 
     public User updateUser(User user) {
-        log.info("Обновление данных пользователя: {}", user);
         return userStorage.updateUser(user);
     }
 
     public Map<String, String> deleteUserById(Integer userId) {
-        log.info("Удаление пользователя по id: {}", userId);
         return userStorage.deleteUserById(userId);
     }
 
     public Map<String, String> deleteAllUsers() {
-        log.info("Удаление всех пользователей");
         return userStorage.deleteAllUsers();
     }
 
     public User addFriend(Integer userId, Integer friendId) {
-        final User user = userStorage.getUserById(userId);
-        final User friend = userStorage.getUserById(friendId);
-        log.info("Пользователь {} добавляет в друзья пользователя {}", user.getName(), friend.getName());
-
-        user.getFriends().add(friendId);
-        friend.getFriends().add(userId);
-        log.info("Пользователи {} и {} стали друзьями", user.getName(), friend.getName());
-
-        userStorage.updateUser(friend);
-        return userStorage.updateUser(user);
+        return userStorage.addFriend(userId, friendId);
     }
 
     public User deleteFriend(Integer userId, Integer friendId) {
-        final User user = userStorage.getUserById(userId);
-        final User friend = userStorage.getUserById(friendId);
-        log.info("Пользователь {} удаляет из друзей пользователя {}", user.getName(), friend.getName());
-
-        user.getFriends().remove(friendId);
-        friend.getFriends().remove(userId);
-        log.info("Пользователи {} и {} больше не друзья", user.getName(), friend.getName());
-
-        userStorage.updateUser(friend);
-        return userStorage.updateUser(user);
+        return userStorage.deleteFriend(userId, friendId);
     }
 
     public List<User> getFriendList(Integer userId) {
-        final User user = userStorage.getUserById(userId);
-        log.info("Получение списка друзей пользователя {}", user.getName());
-
-        if (user.getFriends() == null || user.getFriends().isEmpty()) {
-            log.info("Пользователь пока еще ни с кем не подружился");
-            return List.of();
-        } else {
-            List<User> friendList = new ArrayList<>();
-
-            for (Integer id : user.getFriends()) {
-                friendList.add(userStorage.getUserById(id));
-            }
-
-            log.info("Получен список друзей пользователя {} \n {}", user.getName(), friendList);
-            return friendList;
-        }
+        return userStorage.getFriendList(userId);
     }
 
     public List<User> getCommonFriendList(Integer userId, Integer otherId) {
-        List<User> commonFriends = new ArrayList<>();
-
-        final User user = userStorage.getUserById(userId);
-        final User otherUser = userStorage.getUserById(otherId);
-
-        log.info("Получение списка общих друзей пользователей {} и {}", user.getName(), otherUser.getName());
-        final Set<Integer> userFriends = user.getFriends();
-        final Set<Integer> otherUserFriends = otherUser.getFriends();
-
-        if ((userFriends == null || userFriends.isEmpty()) || (otherUserFriends == null || otherUserFriends.isEmpty())) {
-            log.info("У пользователей {} и {} нет общих друзей", user.getName(), otherUser.getName());
-            return commonFriends;
-        }
-
-        Set<Integer> otherUserFriendsSet = new HashSet<>(userFriends);
-        otherUserFriendsSet.retainAll(otherUserFriends);
-
-        Iterator<Integer> i = otherUserFriendsSet.iterator();
-        while (i.hasNext()) {
-            commonFriends.add(userStorage.getUserById(i.next()));
-        }
-
-        log.info("Получен список общих друзей пользователей {} и {}", user.getName(), otherUser.getName());
-        return commonFriends;
+        return userStorage.getCommonFriendList(userId, otherId);
     }
 
     public void validate(User user, String messagePath) throws ValidationException {
