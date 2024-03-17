74f95f9c5397fbe577ec8676508120288d1d9c1a
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-03-17 04:05:01 +0300

    refactor: спринт 11 (ТЗ проекта) - рефакторинг (улучшить работу блока теста)

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -3,13 +3,16 @@ package ru.yandex.practicum.filmorate.controller;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Qualifier;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.user.UserService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -17,6 +20,8 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
+    @Qualifier("inMemoryUserStorage")
+    private UserStorage userStorage;
     private UserService userService;
     private User user1;
     private User user2;
@@ -28,25 +33,26 @@ class UserControllerTest {
 
     @BeforeEach
     void setUp() {
-        userService = new UserService(new InMemoryUserStorage());
+        userStorage = new InMemoryUserStorage();
+        userService = new UserService(userStorage);
         user1 = new User(1,"test@test.ru",
                 "testLogin", "Test-name",
-                LocalDate.of(2015, 11, 10));
+                LocalDate.of(2015, 11, 10), new HashSet<>());
         user2 = new User(2,"test2@test2.ru",
                 "testLogin2", "Test-name2",
-                LocalDate.of(1994, 5, 12));
+                LocalDate.of(1994, 5, 12), new HashSet<>());
         user3 = new User(3,"test3@test3.ru",
                 "testLogin3", "Test-name3",
-                LocalDate.of(1993, 1, 16));
+                LocalDate.of(1993, 1, 16), new HashSet<>());
         user4 = new User(4,"test4@test4.ru",
                 "testLogin4", "Test-name4",
-                LocalDate.of(1978, 7, 23));
+                LocalDate.of(1978, 7, 23), new HashSet<>());
         user5 = new User(5,"test5@test5.ru",
                 "testLogin5", "Test-name5",
-                LocalDate.of(1996, 11, 20));
+                LocalDate.of(1996, 11, 20), new HashSet<>());
         user6 = new User(6,"test6@test6.ru",
                 "testLogin6", "Test-name6",
-                LocalDate.of(1969, 6, 16));
+                LocalDate.of(1969, 6, 16), new HashSet<>());
     }
 
     @AfterEach
@@ -62,7 +68,7 @@ class UserControllerTest {
 
     @Test
     void createUserInvalid() {
-        final User user = new User();
+        final User user = User.builder().build();
         Exception exc = assertThrows(ValidationException.class, () -> userService.validate(user,
                 "добавить"));
         assertEquals("Не удалось добавить пользователя, т.к. email не заполнено или указано некорректно",
@@ -135,7 +141,7 @@ class UserControllerTest {
 
     @Test
     void updateUserInvalid() {
-        final User user = new User();
+        final User user = User.builder().build();
         Exception exc = assertThrows(ValidationException.class, () -> userService.validate(user,
                 "обновить"));
         assertEquals("Не удалось обновить пользователя, т.к. email не заполнено или указано некорректно",
