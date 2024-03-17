0642b09679f2e9f9d51f91f14870c48c18e6ae9c
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-03-17 04:37:50 +0300

    refactor: спринт 11 (ТЗ проекта) - рефакторинг (изменен запрос на вставку данных в таблицы genres и mpa)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO genres (genre_name) 
-VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+MERGE INTO genres (genre_id, genre_name) 
+VALUES (1, 'Комедия'), (2, 'Драма'), (3, 'Мультфильм'), (4, 'Триллер'), (5, 'Документальный'), (6, 'Боевик');
 
-INSERT INTO mpa (mpa_name, mpa_min_age) 
-VALUES ('G', 0), ('PG', 0), ('PG-13', 13), ('R', 17), ('NC-17', 18);
+MERGE INTO mpa (mpa_id, mpa_name, mpa_min_age) 
+VALUES (1, 'G', 0), (2, 'PG', 0), (3, 'PG-13', 13), (4, 'R', 17), (5, 'NC-17', 18);
