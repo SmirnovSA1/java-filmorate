6ba0d9a96257ebaf8bc4e0761dbfb8fb05122268
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-03-17 03:54:12 +0300

    feat: спринт 11 (ТЗ проекта) - собрать SQL-запросы, добавляющие в БД данные по жанрам и рейтингам возрастного ограничения, в отдельный файл в src/main/resources с названием data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO genres (genre_name) 
+VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+
+INSERT INTO mpa (mpa_name, mpa_min_age) 
+VALUES ('G', 0), ('PG', 0), ('PG-13', 13), ('R', 17), ('NC-17', 18);
